dbea024ed8555b57531a5af22ad59b8481e4745a
atikinkoon <nikita.phone8@gmail.com>
2024-03-27 02:15:50 +0200

    added whole data path

--- labsnlp/CONFIG.py
+++ labsnlp/CONFIG.py
@@ -1,11 +1,12 @@
 import os
 
+WHOLE_DATA_PATH = os.path.join(os.path.dirname(__file__), '../data/sarcasm_detection.csv')
 TRAIN_DATA_PATH = os.path.join(os.path.dirname(__file__), '../data/train.csv')
 TEST_DATA_PATH =  os.path.join(os.path.dirname(__file__), '../data/test.csv')
 
 # PATH_TO_VECTOR =  os.path.join(os.path.dirname(__file__), '../data/glove.6B/glove.6B.50d.txt')
 
-SUBMISSION_DATA_TEST_PATH =  os.path.join(os.path.dirname(__file__), '../data/test.csv')
+SUBMISSION_DATA_TEST_PATH = os.path.join(os.path.dirname(__file__), '../data/test.csv')
 
 OUTPUT_KEY = "is_sarcastic"
 
